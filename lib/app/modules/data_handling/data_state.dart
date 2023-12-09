a83040341c79bb0204ab5c8bc6a61024a34a2199
XimYa <hj_lee_com@naver.com>
2023-12-09 09:58:10 +0900

    feature : 비동기 데이터 상태 관리 모듈 추가
    - 데이터 상태를 간편하고 쉽게 정의하여 비동기 데이터의 상태에 따른 여러 동작과 관련된 로직을 쉽게 처리할 수 있도록 도와줌.

---
+++ lib/app/modules/data_handling/data_state.dart
@@ -0,0 +1,55 @@
+///
+/// 비동기 데이터의 상태를 정의하도록 도와주는 모듈
+///
+
+sealed class Ds<T> {
+  Ds({required this.state, this.error, this.valueOrNull});
+
+  T? valueOrNull;
+  Object? error;
+  DataState state;
+
+  T get value => valueOrNull!;
+
+  R onState<R>({
+    required R Function(T data) fetched,
+    required R Function(Object error) failed,
+    required R Function() loading,
+  }) {
+    if (state.isFailed) {
+      return failed(error!);
+    } else if (state.isLoading) {
+      return loading();
+    } else {
+      return fetched(valueOrNull as T);
+    }
+  }
+}
+
+class Fetched<T> extends Ds<T> {
+  final T data;
+
+  Fetched(this.data) : super(state: DataState.fetched, valueOrNull: data);
+}
+
+class Loading<T> extends Ds<T> {
+  Loading() : super(state: DataState.loading);
+}
+
+class Failed<T> extends Ds<T> {
+  final Object error;
+
+  Failed(this.error) : super(state: DataState.failed, error: error);
+}
+
+enum DataState {
+  fetched,
+  loading,
+  failed;
+
+  bool get isFetched => this == DataState.fetched;
+
+  bool get isLoading => this == DataState.loading;
+
+  bool get isFailed => this == DataState.failed;
+}
