293c51dd441413edcd032d970b0b5b85ac4dcbb0
XimYa <hj_lee_com@naver.com>
2023-12-09 09:57:07 +0900

    feautre : Hex값을 기반으로 앱에서 사용되는 Color 값을 반환하도록 도와주는 모듈 추가

---
+++ lib/app/modules/color/hex_color.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+///
+/// Hex 컬러 값으로 [Color]를 반환받는 모듈
+///
+
+class HexColor extends Color {
+  static int _getColorFromHex(String hexColor) {
+    hexColor = hexColor.toUpperCase().replaceAll("#", "");
+    if (hexColor.length == 6) {
+      hexColor = "FF$hexColor";
+    }
+    return int.parse(hexColor, radix: 16);
+  }
+
+  HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
+}
