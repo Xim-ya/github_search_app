ec479c998f17f6ae4c6b8af5017561eaa7590abb
XimYa <hj_lee_com@naver.com>
2023-12-09 09:45:52 +0900

    chore : 로컬 에셋 경로 정의 모듈 추가

---
+++ lib/app/constant/app_assets.dart
@@ -0,0 +1,33 @@
+///
+/// 로컬 Assets path를 관리하는 클래스
+///
+
+abstract class AppAssets {
+  static const String arrowRightIcon = 'assets/icons/arrow_right.svg';
+  static const String userIcon = 'assets/icons/user.svg';
+  static const String prOpen = 'assets/icons/pr_open.svg';
+  static const String prClosed = 'assets/icons/pr_closed.svg';
+  static const String prDraft = 'assets/icons/pr_draft.svg';
+  static const String prMerged = 'assets/icons/pr_merged.svg';
+  static const String repoIcon = 'assets/icons/repo.svg';
+  static const String longArrowRightIcon = 'assets/icons/long_arrow_right.svg';
+  static const String arrowLeftIcon = 'assets/icons/arrow_left.svg';
+  static const String noResultIndicator =
+      'assets/icons/no_result_indicator.svg';
+  static const String starIcon = 'assets/icons/star.svg';
+  static const String issueIcon = 'assets/icons/issue.svg';
+  static const String commentIcon = 'assets/icons/comment.svg';
+  static const String errorIndicator = 'assets/icons/error_indicator.svg';
+  static const String locationIcon = 'assets/icons/location.svg';
+  static const String followingIcon = 'assets/icons/following.svg';
+  static const String followersIcon = 'assets/icons/followers.svg';
+  static const String calendarIcon = 'assets/icons/calendar.svg';
+  static const String forkIcon = 'assets/icons/fork.svg';
+  static const String issueClosedIcon = 'assets/icons/issue_closed.svg';
+  static const String linkIcon = 'assets/icons/link.svg';
+  static const String starFilledIcon = 'assets/icons/star_filled.svg';
+  static const String githubSplashLottie = 'assets/lottie/github_splash.json';
+  static const String arrowDown = 'assets/icons/arrow_down.svg';
+  static const String noUserIndicator = 'assets/icons/no_user_indicator.svg';
+  static const String exploreIndicator = 'assets/images/explore_indicator.png';
+}
